aeb43b88104be3b3021d939f07007cde9d5b25fc
excretory <elstxds@proton.me>
2025-02-11 13:31:51 +0500

    Created db models and migrations

--- cafe/main/models.py
+++ cafe/main/models.py
@@ -1,3 +1,15 @@
-from django.db import models
 
 # Create your models here.
+from django.db import models
+
+class Item(models.Model):
+    id = models.AutoField('ID', primary_key=True)  # Используем AutoField для автоматической генерации ID
+    name = models.CharField('Название', max_length=100)  # Добавим max_length для поля name
+    price = models.IntegerField('Цена')
+
+class Order(models.Model):
+    id = models.AutoField('ID', primary_key=True)  # Используем AutoField для автоматической генерации ID
+    table_number = models.IntegerField('Номер стола')
+    total_price = models.IntegerField('Общая стоимость')
+    status = models.CharField('Статус', max_length=10)
+    items = models.ManyToManyField(Item, related_name='orders')  # Связь многие ко многим с моделью Item
--- cafe/main/views.py
+++ cafe/main/views.py
@@ -1,6 +1,16 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+
 # Create your views here.
+# TODO Обновить шаблон
 def index(request):
-    return HttpResponse('<h1>Hello world!</h1>')
+    orders = [
+        {'id': 1, 'table_number': 3, 'items': ['coffee', 'cheesecake', 'croissant'], 'total': 40, 'status': 'ready'},
+        {'id': 2, 'table_number': 1, 'items': ['tea', 'burger', 'icecream'], 'total': 27, 'status': 'cooking'}
+    ]
+    data = {
+        'title': 'Cafe',
+        'orders': orders
+    }
+    return render(request, 'main/index.html', data)
