774b9a3254e098e868a8a0d65c18dcb69e670318
excretory <elstxds@proton.me>
2025-02-12 12:26:29 +0500

    Added styles for app

--- cafe/main/views.py
+++ cafe/main/views.py
@@ -6,8 +6,9 @@ from django.shortcuts import render
 # TODO Обновить шаблон
 def index(request):
     orders = [
-        {'id': 1, 'table_number': 3, 'items': ['coffee', 'cheesecake', 'croissant'], 'total': 40, 'status': 'ready'},
-        {'id': 2, 'table_number': 1, 'items': ['tea', 'burger', 'icecream'], 'total': 27, 'status': 'cooking'}
+        {'id': 1, 'table_number': 3, 'items': ['coffee', 'cheesecake', 'croissant'], 'total': 40, 'status': 'готово'},
+        {'id': 2, 'table_number': 1, 'items': ['tea', 'burger', 'icecream'], 'total': 27, 'status': 'в ожидании'},
+        {'id': 3, 'table_number': 7, 'items': ['tea', 'burger', 'icecream'], 'total': 64, 'status': 'оплачено'}
     ]
     data = {
         'title': 'Cafe',
