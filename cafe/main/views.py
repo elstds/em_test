d29d41bcc2ff0069e715add9c91029dff0c7b739
excretory <elstxds@proton.me>
2025-02-25 10:10:12 +0500

    forms for orders

--- cafe/main/views.py
+++ cafe/main/views.py
@@ -1,15 +1,10 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Order
 
 
-# Create your views here.
-# TODO Обновить шаблон
 def index(request):
-    orders = [
-        {'id': 1, 'table_number': 3, 'items': ['coffee', 'cheesecake', 'croissant'], 'total': 40, 'status': 'готово'},
-        {'id': 2, 'table_number': 1, 'items': ['tea', 'burger', 'icecream'], 'total': 27, 'status': 'в ожидании'},
-        {'id': 3, 'table_number': 7, 'items': ['tea', 'burger', 'icecream'], 'total': 64, 'status': 'оплачено'}
-    ]
+    orders = Order.objects.all()
     data = {
         'title': 'Cafe',
         'orders': orders
