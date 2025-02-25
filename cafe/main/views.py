b23897f27e4faaaf916cb7f7e2dd416a8e3f9169
excretory <elstxds@proton.me>
2025-02-25 14:01:23 +0500

    todo

--- cafe/main/views.py
+++ cafe/main/views.py
@@ -1,12 +1,14 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-from .models import Order
+from .models import Order, Item
 
 
 def index(request):
     orders = Order.objects.all()
+    items = Item.objects.all()
     data = {
         'title': 'Cafe',
-        'orders': orders
+        'orders': orders,
+        'items': items
     }
     return render(request, 'main/index.html', data)
